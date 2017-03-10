afdb812c620d720be9e0a9e01d8159090a0a2001
Vaughn Kottler <vkottler@wisc.edu>
2017-03-09 20:56:43 -0600

    updating team page

--- js/main.js
+++ js/main.js
@@ -8,8 +8,7 @@ angular.module('controllers', [])
         { text: "contact",      enabled: false },
         { text: "sponsorship",  enabled: false },
         { text: "media",        enabled: false },
-        { text: "team",         enabled: false },
-        { text: "page",         enabled: true  }
+        { text: "team",         enabled: true  }
     ];
     $scope.homeButton = { text: "home", enabled: false };
     $scope.curr_button = $scope.buttons[0];
